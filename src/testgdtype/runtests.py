f7499b7d2ff5095b0eccd627d4e0c76739c1317e
anetczuk <anetczuk@o2.pl>
2022-10-10 00:40:11 +0200

    Update license

--- src/testgdtype/runtests.py
+++ src/testgdtype/runtests.py
@@ -2,7 +2,7 @@
 #
 # MIT License
 #
-# Copyright (c) 2020 Arkadiusz Netczuk <dev.arnet@gmail.com>
+# Copyright (c) 2022 Arkadiusz Netczuk <dev.arnet@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
