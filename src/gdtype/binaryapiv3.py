3798d38c06f56cbfc1e6191c430294cb99fa6c56
anetczuk <anetczuk@o2.pl>
2022-10-09 23:13:23 +0200

    Refactor

--- src/gdtype/binaryapiv3.py
+++ src/gdtype/binaryapiv3.py
@@ -24,73 +24,24 @@
 import logging
 from enum import IntEnum, unique
 
-from typing import List, Dict, Tuple, Callable, Any
-from types import FunctionType
+from typing import List, Tuple
 
-from .bytescontainer import BytesContainer
 from . import commontypes as ct
 
 
 _LOGGER = logging.getLogger(__name__)
 
 
-def deserialize( message: bytes ):
-    mess_len = len( message )
-    if mess_len < 4:
-        _LOGGER.error( "invalid packet -- too short: %s", message )
-        raise ValueError( f"invalid packet -- too short: {mess_len} < 4 for {message!r}" )
-
-    data = BytesContainer( message )
-    expected_size = data.popInt32()
-    message_size  = data.size()
-    if message_size != expected_size:
-        _LOGGER.error( "invalid packet -- packet size mismatch data size: %s", data )
-        raise ValueError( f"message size mismatch: {message_size} != {expected_size} for {data}" )
-
-    return ct.deserialize_type( data )
-
-
-def serialize( value ) -> bytes:
-    data = BytesContainer()
-    ct.serialize_type( value, data )
-    data_size = data.size()
-    if data_size < 1:
-        ## failed to serialize data
-        raise ValueError( "failed to serialize: empty output data" )
-    message = BytesContainer()
-    message.pushInt32( data_size )        ## set header
-    message.push( data.data )
-    return message.data
-
-
-## read header value of message and return it
-def get_message_length( data: bytes ):
-    container = BytesContainer( data )
-    if container.size() < 4:
-        _LOGGER.error( "message is too short: %s", data )
-        return None
-    return container.popInt32()
-
-
-## return:
-##        0         if buffer contains full message without additional bytes
-##        negative  if there is missing data (how many bytes are missing)
-##        positive  if there are additional bytes (part of next message in case of TCP/IP)
 ##
-## whole message consists of header and remaining data
-## header is 4 bytes integer containing size of remaining data
-## so whole message is sum of value in header increased by size of header itself
-def check_message_size( buffer: bytes ):
-    curr_size = len( buffer )
-    if curr_size < 4:
-        return -4 + curr_size
-    expected_size = get_message_length( buffer )
-    expected_size += 4                              ## increase by message header
-    return curr_size - expected_size
+## interface functions
+##
+deserialize        = ct.deserialize
+serialize          = ct.serialize
+get_message_length = ct.get_message_length
+check_message_size = ct.check_message_size
 
 
-## ======================================================================
-## ======================================================================
+## ============================================================
 
 
 @unique
@@ -174,33 +125,7 @@ CONFIG_LIST: List[ Tuple ] = [
 ## ======================================================================
 
 
-## calculate proper maps and validate configuration
-DESERIALIZATION_MAP: Dict[ int, Callable[[int, BytesContainer], Any] ] = {}
-SERIALIZATION_MAP: Dict[ object, Tuple[int, FunctionType] ] = {}
-# SERIALIZATION_MAP: Dict[ object, Tuple[int, Callable[[int, Any, BytesContainer], Any]] ] = {}
-
-for config in CONFIG_LIST:
-    ## deserialization map
-    config_gd_type: int  = config[0]
-    config_py_type: type = config[1]
-
-    if len( config ) == 4:
-        deserialize_func: FunctionType = config[2]
-        serialize_func: FunctionType   = config[3]
-    else:
-        ## find serialization/deserialization functions by name
-        py_type_name = config_py_type.__name__
-        deserialize_func = getattr( ct, f"deserialize_{py_type_name}" )
-        serialize_func   = getattr( ct, f"serialize_{py_type_name}" )
-
-    if config_gd_type in DESERIALIZATION_MAP:
-        raise ValueError( f"invalid CONFIG_LIST: Godot type {config_gd_type} already defined" )
-    DESERIALIZATION_MAP[ config_gd_type ] = deserialize_func
-
-    ## serialization map
-    if config_py_type in SERIALIZATION_MAP:
-        raise ValueError( f"invalid CONFIG_LIST: Python type {config_py_type} already defined" )
-    SERIALIZATION_MAP[ config_py_type ] = ( config_gd_type, serialize_func )
+DESERIALIZATION_MAP, SERIALIZATION_MAP = ct.prepare_config_dicts( CONFIG_LIST, ct )
 
 
 def get_deserialization_function_v3( gd_type_id: int ):
@@ -214,5 +139,6 @@ def get_serialization_config_v3( py_type: type ):
     return SERIALIZATION_MAP.get( py_type, None )
 
 
+## override 'abstract' functions
 ct.get_deserialization_function = get_deserialization_function_v3
 ct.get_serialization_config     = get_serialization_config_v3
--- src/gdtype/commontypes.py
+++ src/gdtype/commontypes.py
@@ -27,11 +27,72 @@ import logging
 from dataclasses import dataclass, field
 
 from typing import List, Tuple
+from typing import Dict, Callable, Any
+from types import FunctionType
 
 from .bytescontainer import BytesContainer
 
 
 _LOGGER = logging.getLogger(__name__)
+
+
+def deserialize( message: bytes ):
+    mess_len = len( message )
+    if mess_len < 4:
+        _LOGGER.error( "invalid packet -- too short: %s", message )
+        raise ValueError( f"invalid packet -- too short: {mess_len} < 4 for {message!r}" )
+
+    data = BytesContainer( message )
+    expected_size = data.popInt32()
+    message_size  = data.size()
+    if message_size != expected_size:
+        _LOGGER.error( "invalid packet -- packet size mismatch data size: %s", data )
+        raise ValueError( f"message size mismatch: {message_size} != {expected_size} for {data}" )
+
+    return deserialize_type( data )
+
+
+def serialize( value ) -> bytes:
+    data = BytesContainer()
+    serialize_type( value, data )
+    data_size = data.size()
+    if data_size < 1:
+        ## failed to serialize data
+        raise ValueError( "failed to serialize: empty output data" )
+    message = BytesContainer()
+    message.pushInt32( data_size )        ## set header
+    message.push( data.data )
+    return message.data
+
+
+## read header value of message and return it
+def get_message_length( data: bytes ):
+    container = BytesContainer( data )
+    if container.size() < 4:
+        _LOGGER.error( "message is too short: %s", data )
+        return None
+    return container.popInt32()
+
+
+## return:
+##        0         if buffer contains full message without additional bytes
+##        negative  if there is missing data (how many bytes are missing)
+##        positive  if there are additional bytes (part of next message in case of TCP/IP)
+##
+## whole message consists of header and remaining data
+## header is 4 bytes integer containing size of remaining data
+## so whole message is sum of value in header increased by size of header itself
+def check_message_size( buffer: bytes ):
+    curr_size = len( buffer )
+    if curr_size < 4:
+        return -4 + curr_size
+    expected_size = get_message_length( buffer )
+    expected_size += 4                              ## increase by message header
+    return curr_size - expected_size
+
+
+## ======================================================================
+## ======================================================================
 
 
 ## returns Python equivalent of given data
@@ -1299,3 +1360,41 @@ def serialize_ColorArray( gd_type_id: int, value: ColorArray, data: BytesContain
         data.pushFloat32( item[1] )
         data.pushFloat32( item[2] )
         data.pushFloat32( item[3] )
+
+
+## ======================================================================
+
+
+def prepare_config_dicts( config_list, module ):
+    ## calculate proper maps and validate configuration
+    DESERIALIZATION_MAP: Dict[ int, Callable[[int, BytesContainer], Any] ] = {}
+    SERIALIZATION_MAP: Dict[ object, Tuple[int, FunctionType] ] = {}
+    # SERIALIZATION_MAP: Dict[ object, Tuple[int, Callable[[int, Any, BytesContainer], Any]] ] = {}
+
+    for config in config_list:
+        ## deserialization map
+        config_gd_type: int  = config[0]
+        config_py_type: type = config[1]
+
+        if len( config ) == 4:
+            deserialize_func: FunctionType = config[2]
+            serialize_func: FunctionType   = config[3]
+        else:
+            ## find serialization/deserialization functions by name
+            py_type_name = config_py_type.__name__
+            deserialize_func = getattr( module, f"deserialize_{py_type_name}" )
+            serialize_func   = getattr( module, f"serialize_{py_type_name}" )
+#             mod_vars = vars()
+#             deserialize_func = mod_vars[ f"deserialize_{py_type_name}" ]
+#             serialize_func   = mod_vars[ f"serialize_{py_type_name}" ]
+
+        if config_gd_type in DESERIALIZATION_MAP:
+            raise ValueError( f"invalid CONFIG_LIST: Godot type {config_gd_type} already defined" )
+        DESERIALIZATION_MAP[ config_gd_type ] = deserialize_func
+
+        ## serialization map
+        if config_py_type in SERIALIZATION_MAP:
+            raise ValueError( f"invalid CONFIG_LIST: Python type {config_py_type} already defined" )
+        SERIALIZATION_MAP[ config_py_type ] = ( config_gd_type, serialize_func )
+
+    return ( DESERIALIZATION_MAP, SERIALIZATION_MAP )
